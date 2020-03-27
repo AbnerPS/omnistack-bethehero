6dc9e204606ab1c2f502624a187f3da5730e85b6
Abner Silva <link_abner.p.s@hotmail.com>
2020-03-27 19:52:16 -0300

    Developed application mobile

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react'
+import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons'
+import logoImg from '../../assets/logo.png'
+import api from '../../services/api'
+import styles from './styles'
+
+export default function Incidents() {
+    const navigation = useNavigation()
+    const [incidents, setIncidents] = useState([])
+    const [total, setTotal] = useState(0)
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+
+    async function loadIncidents() {
+        if (loading){
+            return
+        }
+
+        if (total > 0 && incidents.length == total) {
+            return
+        }
+
+        setLoading(true)
+
+        const response = await api.get('incidents', {
+            params: { page }
+        })
+
+        setIncidents([...incidents, ...response.data])
+        setTotal(response.headers['x-total-count'])
+        setPage(page + 1)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        loadIncidents()
+    }, [])
+
+    function navigateToDetail(incident) {
+        navigation.navigate('Detail', { incident })
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}> Total de 
+                    <Text style={styles.headerTextBold}> {total} casos</Text>
+                </Text>
+            </View>
+
+            <Text style={styles.title}>Sejá Bem-Vindo!</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia como um herói</Text>
+
+            <FlatList
+            data={incidents}
+            style={styles.incidentsList}
+            keyExtractor={incident => String(incident.id)}
+            showsVerticalScrollIndicator={false}
+            onEndReached={loadIncidents}
+            onEndReachedThreshold={0.2}
+            renderItem={({ item: incident }) => (
+                <View style={styles.incident}>
+                    <Text style={styles.incidentProperty}>ONG:</Text>
+                <Text style={styles.incidentValue}>{incident.name}</Text>
+
+                    <Text style={styles.incidentProperty}>CASO:</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
+
+                    <Text style={styles.incidentProperty}>VALOR:</Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR',
+                    {style: 'currency',
+                    currency: 'BRL'}).format(incident.value)}
+                </Text>
+
+                    <TouchableOpacity
+                    style={styles.detailButton}
+                    onPress={() => navigateToDetail(incident)}>
+                        <Text style={styles.detailButtonText}>Ver mais detalhes</Text>
+                        <Feather name="arrow-right" size={16} color="#fff"/>
+                    </TouchableOpacity>
+                </View>
+            )}
+            />
+        </View>
+    )
+}
---
+++ mobile/src/pages/Incidents/styles.js
@@ -0,0 +1,80 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    headerText: {
+        fontSize: 15,
+        color: '#737380'
+    },
+
+    headerTextBold: {
+        fontWeight: 'bold'
+    },
+
+    title: {
+        fontSize: 30,
+        marginBottom: 16,
+        marginTop: 48,
+        color: '#13131a',
+        fontWeight: 'bold'
+    },
+
+    description: {
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#737380'
+    },
+
+    incidentsList: {
+        marginTop: 32
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom: 16
+    },
+
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold'
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
+
+    detailButton: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e02041',
+        borderRadius: 8,
+        height: 40,
+        width: '100%'
+    },
+
+    detailButtonText: {
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold',
+        paddingRight: 10
+    }
+})
